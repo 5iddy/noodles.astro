ee314531d2b1f3f92a086aa050534e4a87f11634
5iddy <siddhuyeturi@gmail.com>
2023-04-04 01:53:34 +0530

    Added Treesitter playground and modified cmp setup

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,176 +1,174 @@
-local utils = require "astronvim.utils"
-
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "FelipeLema/cmp-async-path",
-        "octaltree/cmp-look",
-        "hrsh7th/cmp-calc",
-        "f3fora/cmp-spell",
-        {
-            "uga-rosa/cmp-dictionary",
-            opts = {
-                dic = { ["*"] = "/home/noodles/dictionaries/en_us.dict" },
-            },
-        },
-        "chrisgrieser/cmp-nerdfont",
-        "hrsh7th/cmp-emoji",
-        "L3MON4D3/LuaSnip",
-        { "onsails/lspkind.nvim" },
-        {
-            "tamago324/cmp-zsh",
-            opts = { zshrc = true, filetypes = { "zsh" } },
-        },
-        {
-            "tzachar/cmp-fuzzy-buffer",
-            dependencies = {
-                "hrsh7th/nvim-cmp",
-                "tzachar/fuzzy.nvim",
-            },
-        },
-        "lukas-reineke/cmp-rg",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-nvim-lsp-document-symbol",
-        {
-            "garyhurtz/cmp_bulma.nvim",
-            config = function() require("cmp_bulma"):setup() end,
-        },
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "FelipeLema/cmp-async-path",
+    "octaltree/cmp-look",
+    "hrsh7th/cmp-calc",
+    "f3fora/cmp-spell",
+    {
+      "uga-rosa/cmp-dictionary",
+      opts = {
+        dic = { ["*"] = "/home/noodles/dictionaries/en_us.dict" },
+      },
     },
-    opts = function(_, opts)
-        local cmp = require "cmp"
-        local kind_icons = {
-            Text = "",
-            Method = "",
-            Function = "",
-            Constructor = "",
-            Field = "",
-            Variable = "",
-            Class = "ﴯ",
-            Interface = "",
-            Module = "",
-            Property = "ﰠ",
-            Unit = "",
-            Value = "",
-            Enum = "",
-            Keyword = "",
-            Snippet = "",
-            Color = "",
-            File = "",
-            Reference = "",
-            Folder = "",
-            EnumMember = "",
-            Constant = "",
-            Struct = "",
-            Event = "",
-            Operator = "",
-            TypeParameter = "",
-        }
+    "chrisgrieser/cmp-nerdfont",
+    "hrsh7th/cmp-emoji",
+    "L3MON4D3/LuaSnip",
+    { "onsails/lspkind.nvim" },
+    {
+      "tamago324/cmp-zsh",
+      opts = { zshrc = true, filetypes = { "zsh" } },
+    },
+    {
+      "tzachar/cmp-fuzzy-buffer",
+      dependencies = {
+        "hrsh7th/nvim-cmp",
+        "tzachar/fuzzy.nvim",
+      },
+    },
+    "lukas-reineke/cmp-rg",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    {
+      "garyhurtz/cmp_bulma.nvim",
+      config = function() require("cmp_bulma"):setup() end,
+    },
+  },
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    local kind_icons = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "",
+      Event = "",
+      Operator = "",
+      TypeParameter = "",
+    }
 
-        opts.formatting = {
-            format = function(entry, vim_item)
-                -- Kind icons
-                vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-                -- Source
-                vim_item.menu = ({
-                        buffer = "﬘ ",
-                        nvim_lsp = " ",
-                        luasnip = " ",
-                        nvim_lua = " ",
-                        latex_symbols = "🔣",
-                        fuzzy_buffer = "🔎 ",
-                        async_path = "ﱮ /",
-                        look = "👀",
-                        spell = "🔤",
-                        dictionary = "📖",
-                        nerdfont = " ",
-                        emoji = "😄",
-                        calc = " ",
-                        rg = "",
-                    })[entry.source.name]
-                return vim_item
-            end,
-        }
+    opts.formatting = {
+      format = function(entry, vim_item)
+        -- Kind icons
+        vim_item.kind = string.format("%s", kind_icons[vim_item.kind]) -- This concatonates the icons with the name of the item kind
+        -- Source
+        vim_item.menu = ({
+          buffer = "﬘ ",
+          nvim_lsp = " ",
+          luasnip = " ",
+          nvim_lua = " ",
+          latex_symbols = "🔣",
+          fuzzy_buffer = "🔎 ",
+          async_path = "ﱮ /",
+          look = "👀",
+          spell = "🔤",
+          dictionary = "📖",
+          nerdfont = " ",
+          emoji = "😄",
+          calc = " ",
+          rg = "",
+        })[entry.source.name]
+        return vim_item
+      end,
+    }
 
-        opts.sources = cmp.config.sources {
-                { name = "nvim_lsp", priority = 1000 },
-                { name = "luasnip",  priority = 750 },
-                {
-                    name = "fuzzy_buffer",
-                    priority = 500,
-                    keyword_length = 3,
-                },
-                {
-                    name = "async_path",
-                    priority = 250,
-                    keyword_length = 3,
-                },
-                { name = "nvim_lua", priority = 600 },
-                {
-                    name = "buffer",
-                    priority = 250,
-                    keyword_length = 3,
-                },
-                { name = "calc",     priority = 100 },
-                {
-                    name = "look",
-                    option = { convert_case = true, loud = true },
-                    priority = 175,
-                    keyword_length = 3,
-                },
-                {
-                    name = "spell",
-                    option = { keep_all_entries = false },
-                    keyword_length = 3,
-                    priority = 225,
-                },
-                {
-                    name = "dictionary",
-                    priority = 200,
-                    keyword_length = 3,
-                },
-                {
-                    name = "nerdfont",
-                    priority = 375,
-                },
-                { name = "emoji", priority = 325 },
-                { name = "zsh",   priority = 300 },
-                { name = "rg",    keyword_length = 3, priority = 400 },
-                {
-                    name = "bulma",
-                    keyword_length = 3,
-                },
-            }
-        opts.window = {
-            completion = cmp.config.window.bordered(),
-            documentation = cmp.config.window.bordered(),
-        }
-        -- `/` cmdline setup.
-        cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-                { name = "rg" },
-                { name = "fuzzy_buffer" },
-                { name = "nvim_lsp_document_symbol" },
-            },
-        })
-        -- `:` cmdline setup.
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-            }, {
-                {
-                    name = "cmdline",
-                    option = {
-                        ignore_cmds = { "Man", "!" },
-                    },
-                },
-            }),
-        })
-        return opts
-    end,
+    opts.sources = cmp.config.sources {
+      { name = "nvim_lsp", priority = 1000 },
+      { name = "luasnip", priority = 750 },
+      {
+        name = "fuzzy_buffer",
+        priority = 500,
+        keyword_length = 3,
+      },
+      {
+        name = "async_path",
+        priority = 250,
+        keyword_length = 3,
+      },
+      { name = "nvim_lua", priority = 600 },
+      {
+        name = "buffer",
+        priority = 250,
+        keyword_length = 3,
+      },
+      { name = "calc", priority = 100 },
+      {
+        name = "look",
+        option = { convert_case = true, loud = true },
+        priority = 175,
+        keyword_length = 3,
+      },
+      {
+        name = "spell",
+        option = { keep_all_entries = false },
+        keyword_length = 3,
+        priority = 225,
+      },
+      {
+        name = "dictionary",
+        priority = 200,
+        keyword_length = 3,
+      },
+      {
+        name = "nerdfont",
+        priority = 375,
+      },
+      { name = "emoji", priority = 325 },
+      { name = "zsh", priority = 300 },
+      { name = "rg", keyword_length = 3, priority = 400 },
+      {
+        name = "bulma",
+        keyword_length = 3,
+      },
+    }
+    opts.window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    }
+    -- `/` cmdline setup.
+    cmp.setup.cmdline("/", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+        { name = "rg" },
+        { name = "fuzzy_buffer" },
+        { name = "nvim_lsp_document_symbol" },
+      },
+    })
+    -- `:` cmdline setup.
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+      }, {
+        {
+          name = "cmdline",
+          option = {
+            ignore_cmds = { "Man", "!" },
+          },
+        },
+      }),
+    })
+    return opts
+  end,
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+    keys = {
+      { "<leader>Pt", "<CMD>TSPlaygroundToggle<CR>", desc = "UI toggle Tree Sitter playground" },
+      { "<leader>Pc", "<CMD>TSHighlightCapturesUnderCursor<CR>", desc = "Tree Sitter Highlight Captures Under Cursor" },
+      { "<leader>Pn", "<CMD>TSNodeUnderCursor<CR>", desc = "Tree Sitter Node Under Cursor" },
+    },
+  },
   opts = {
     ensure_installed = {
       "help",
@@ -29,6 +38,7 @@ return {
       "vim",
       "vue",
       "typescript",
+      "query",
     },
     auto_install = true,
     highlight = {
@@ -43,6 +53,29 @@ return {
       enable = true,
       extended_mode = true,
       max_file_lines = nil,
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = "o",
+        toggle_hl_groups = "i",
+        toggle_injected_languages = "t",
+        toggle_anonymous_nodes = "a",
+        toggle_language_display = "I",
+        focus_language = "f",
+        unfocus_language = "F",
+        update = "R",
+        goto_node = "<cr>",
+        show_help = "?",
+      },
+    },
+    query_linter = {
+      enable = true,
+      use_virtual_text = true,
+      lint_events = { "BufWrite", "CursorHold" },
     },
   },
 }
